46d78d506bd1944f8161b8398efd727e2decb046
amit <amitsrivastava200892@gmail.com>
2017-11-19 15:43:14 +0530

    fixing .gitignore

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="6b0e254b-cb5a-4a7a-b942-ccd7004804ae")
-public class DataBindingInfo {}
