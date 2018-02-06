24bb40a3264c1a4213f0400d494aa19351cfa50d
ArifMohd <arif68411@gmail.com>
2018-02-06 15:46:59 +0530

    minor changes

--- app/src/main/java/com/cncoding/teazer/ui/fragment/fragment/FragmentReactionplayer.java
+++ app/src/main/java/com/cncoding/teazer/ui/fragment/fragment/FragmentReactionplayer.java
@@ -219,6 +219,7 @@ public class FragmentReactionplayer extends BaseFragment {
                 try {
 
                     videoURL = postDetails.getMediaDetail().getMediaUrl();
+
                     if (postDetails != null) {
                         reactId = postDetails.getReactId();
                         isLiked = !postDetails.canLike();
@@ -239,10 +240,8 @@ public class FragmentReactionplayer extends BaseFragment {
                             }
                             reactionPostCaption.setText(reactionTitle);
                         }
-
                         postDurationView.setText(postDetails.getMediaDetail().getReactDuration());
                         reactionPostName.setText(postDetails.getReactOwner().getFirstName());
-
 
                         ApiCallingService.Posts.getPostDetails(postDetails.getPostId(), context)
                                 .enqueue(new Callback<PostDetails>() {
